1588d24b41cda0e813bf97ae9e8f7cfe541c5270
Dan Fyfe <fyfedaniel@gmail.com>
2024-02-22 15:03:53 -0500

    start variable data proj

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -1,4 +1,14 @@
-﻿string first = "Hello";
-string second = "World";
-string result = string.Format("{0} {1}!", first, second);
-Console.WriteLine(result);
+﻿string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+Console.WriteLine(comparisonMessage);
+
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+Console.WriteLine(comparisonMessage);
+
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+Console.WriteLine(comparisonMessage);
