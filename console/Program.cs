36439c30ff16714373432b2c385f6ba142b844b1
Dan Fyfe <fyfedaniel@gmail.com>
2024-02-23 13:09:56 -0500

    error challenge

--- console/Program.cs
+++ console/Program.cs
@@ -1,50 +1,41 @@
-﻿checked
+﻿string[][] userEnteredValues = new string[][]
+{
+        new string[] { "1", "two", "3"},
+        new string[] { "0", "1", "2"}
+};
+
+foreach (string[] userEntries in userEnteredValues)
 {
   try
   {
-    int num1 = int.MaxValue;
-    int num2 = int.MaxValue;
-    int result = num1 + num2;
-    Console.WriteLine("Result: " + result);
+    BusinessProcess1(userEntries);
   }
-  catch (OverflowException ex)
+  catch (Exception ex)
   {
-    Console.WriteLine("Error: The number is too large to be represented as an integer. " + ex.Message);
+    if (ex.StackTrace.Contains("BusinessProcess1") && (ex is FormatException))
+    {
+      Console.WriteLine(ex.Message);
+    }
   }
 }
 
-try
+static void BusinessProcess1(string[] userEntries)
 {
-  string? str = null;
-  int length = str.Length;
-  Console.WriteLine("String Length: " + length);
-}
-catch (NullReferenceException ex)
-{
-  Console.WriteLine("Error: The reference is null. " + ex.Message);
-}
+  int valueEntered;
 
-try
-{
-  int[] numbers = new int[5];
-  numbers[5] = 10;
-  Console.WriteLine("Number at index 5: " + numbers[5]);
-}
-catch (IndexOutOfRangeException ex)
-{
-  Console.WriteLine("Error: Index out of range. " + ex.Message);
-}
+  foreach (string userValue in userEntries)
+  {
+    try
+    {
+      valueEntered = int.Parse(userValue);
 
-try
-{
-  int num3 = 10;
-  int num4 = 0;
-  int result2 = num3 / num4;
-  Console.WriteLine("Result: " + result2);
+      // completes required calculations based on userValue
+      // ...
+    }
+    catch (FormatException)
+    {
+      FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+      throw invalidFormatException;
+    }
+  }
 }
-catch (DivideByZeroException ex)
-{
-  Console.WriteLine("Error: Cannot divide by zero. " + ex.Message);
-}
-
-Console.WriteLine("Exiting program.");
